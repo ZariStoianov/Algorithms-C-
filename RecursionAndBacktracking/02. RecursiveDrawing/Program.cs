0651230cffd55ae377910325035e53505c7abcd5
Zari <zari_stoqnov@abv.bg>
2022-06-27 22:57:53 +0300

    DrawFigure

--- RecursionAndBacktracking/02. RecursiveDrawing/Program.cs
+++ RecursionAndBacktracking/02. RecursiveDrawing/Program.cs
@@ -2,11 +2,27 @@
 {
     using System;
 
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var number = int.Parse(Console.ReadLine());
+
+            DrawFigure(number);
+        }
+
+        private static void DrawFigure(int number)
+        {
+            if (number == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new String('*', number));
+
+            DrawFigure(number - 1);
+
+            Console.WriteLine(new String('#', number));
         }
     }
 }
