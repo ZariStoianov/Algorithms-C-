e9040f57657a5b94e45dfcf3d764a7eb5b5c7e59
Zari <zari_stoqnov@abv.bg>
2022-06-27 23:12:29 +0300

    Fibonacci

--- RecursionAndBacktracking/06. 8 QueensPuzzle/Program.cs
+++ RecursionAndBacktracking/06. 8 QueensPuzzle/Program.cs
@@ -1,12 +1,82 @@
 ﻿namespace _06._8_QueensPuzzle
 {
     using System;
+    using System.Collections.Generic;
 
-    internal class Program
+    public class Program
     {
+        private static HashSet<int> attackedRows = new HashSet<int>();
+        private static HashSet<int> attackedCols = new HashSet<int>();
+        private static HashSet<int> attackedLeftDiagonals = new HashSet<int>();
+        private static HashSet<int> attackedRightDiagonals = new HashSet<int>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var board = new bool[8, 8];
+
+            PutQueens(board, 0);
+            for (int i = 0; i < 8; i++)
+            {
+
+            }
+        }
+
+        private static void PutQueens(bool[,] board, int row)
+        {
+
+            if (row >= board.GetLength(0))
+            {
+                PrintBoard(board);
+                return;
+            }
+            for (int col = 0; col < board.GetLength(1); col++)
+            {
+                if (CanPlaceQueens(row, col))
+                {
+                    attackedRows.Add(row);
+                    attackedCols.Add(col);
+                    attackedLeftDiagonals.Add(row - col);
+                    attackedRightDiagonals.Add(row + col);
+                    board[row, col] = true;
+
+                    PutQueens(board, row + 1);
+
+                    attackedRows.Remove(row);
+                    attackedCols.Remove(col);
+                    attackedLeftDiagonals.Remove(row - col);
+                    attackedRightDiagonals.Remove(row + col);
+                    board[row, col] = false;
+                }
+            }
+        }
+
+        private static void PrintBoard(bool[,] board)
+        {
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    if (board[row, col])
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("- ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+
+        private static bool CanPlaceQueens(int row, int col)
+        {
+            return !attackedRows.Contains(row) &&
+                   !attackedCols.Contains(col) &&
+                   !attackedLeftDiagonals.Contains(row - col) &&
+                   !attackedRightDiagonals.Contains(row + col);
         }
     }
 }
---
+++ RecursionAndBacktracking/07. RecursiveFibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _07._RecursiveFibonacci
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            CalcFibonacci(number);
+
+            Console.WriteLine(CalcFibonacci(number));
+        }
+
+        private static int CalcFibonacci(int number)
+        {
+            if (number <= 1 || number >= 45)
+            {
+                return 1;
+            }
+
+            return CalcFibonacci(number - 1) + CalcFibonacci(number - 2);
+        }
+    }
+}
