89fe35af1f0b602665e28080bf69d12d7b0604bf
Zari <zari_stoqnov@abv.bg>
2022-06-27 23:08:39 +0300

    Puzzle

--- RecursionAndBacktracking/05. PathsInLabyrinth/Program.cs
+++ RecursionAndBacktracking/05. PathsInLabyrinth/Program.cs
@@ -1,12 +1,62 @@
 ﻿namespace _05._PathsInLabyrinth
 {
     using System;
+    using System.Collections.Generic;
 
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rows = int.Parse(Console.ReadLine());
+            var cols = int.Parse(Console.ReadLine());
+
+            var lab = new char[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var colElements = Console.ReadLine();
+
+                for (int j = 0; j < cols; j++)
+                {
+                    lab[i, j] = colElements[j];
+                }
+            }
+
+            FindPath(lab, 0, 0, new List<string>(), string.Empty);
+        }
+
+        private static void FindPath(char[,] lab, int row, int col, List<string> directions, string direction)
+        {
+            //validate row, col
+            if (row < 0 || row >= lab.GetLength(0) || col < 0 || col >= lab.GetLength(1))
+            {
+                return;
+            }
+
+            //check for walls or already visited cells
+            if (lab[row, col] == '*' || lab[row, col] == 'v')
+            {
+                return;
+            }
+
+            directions.Add(direction);
+            //check for end
+            if (lab[row, col] == 'e')
+            {
+                Console.WriteLine(string.Join(string.Empty, directions));
+                directions.RemoveAt(directions.Count - 1);
+                return;
+            }
+
+            lab[row, col] = 'v';
+
+            FindPath(lab, row - 1, col, directions, "U");// up
+            FindPath(lab, row + 1, col, directions, "D");// down
+            FindPath(lab, row, col - 1, directions, "L");// left
+            FindPath(lab, row, col + 1, directions, "R");// right
+
+            lab[row, col] = '-';
+            directions.RemoveAt(directions.Count - 1);
         }
     }
 }
---
+++ RecursionAndBacktracking/06. 8 QueensPuzzle/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _06._8_QueensPuzzle
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
