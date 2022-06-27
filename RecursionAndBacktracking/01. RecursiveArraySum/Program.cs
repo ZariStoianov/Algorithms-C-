f2baeeb121ae54b051105d0322479c6842b958cf
Zari <zari_stoqnov@abv.bg>
2022-06-27 22:44:03 +0300

    ArraySum

---
+++ RecursionAndBacktracking/01. RecursiveArraySum/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01._RecursiveArraySum
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(GetSum(numbers, 0));
+        }
+
+        private static int GetSum(int[] numbers, int index)
+        {
+            if (index >= numbers.Length)
+            {
+                return 0;
+            }
+
+            return numbers[index] + GetSum(numbers, index + 1);
+        }
+    }
+}
