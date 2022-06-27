fea232c1c6e5c55714404b47d1f02ed96d0daea5
Zari <zari_stoqnov@abv.bg>
2022-06-27 23:03:37 +0300

    Factorial

---
+++ RecursionAndBacktracking/04. RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _04._RecursiveFactorial
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CalcFactorial(n));
+        }
+
+        private static int CalcFactorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * CalcFactorial(n - 1);
+        }
+    }
+}
