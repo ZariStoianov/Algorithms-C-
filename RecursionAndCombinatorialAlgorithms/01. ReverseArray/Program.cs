81d0b2c6c44eab43d4d34bbd4db8c2f228a2ee69
Zari <zari_stoqnov@abv.bg>
2022-07-01 11:26:31 +0300

    ReverseArray

---
+++ RecursionAndCombinatorialAlgorithms/01. ReverseArray/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01._ReverseArray
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var elements = Console.ReadLine().Split();
+
+            Reverse(elements, 0);
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+
+        private static void Reverse(string[] elements, int index)
+        {
+
+            if (index == elements.Length / 2)
+            {
+                return;
+            }
+            var temp = elements[index]; 
+            elements[index] = elements[elements.Length - index - 1]; 
+            elements[elements.Length - index - 1] = temp;
+
+            Reverse(elements, index + 1);
+        }
+    }
+}
