5dc078f6ec6ddb8d105e5706963e2e200e175001
Zari <zari_stoqnov@abv.bg>
2022-07-01 16:32:02 +0300

    ConnectedAreas

---
+++ RecursionAndCombinatorialAlgorithms/02. NestedLoops/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02._NestedLoops
+{
+    using System;
+
+    public class Program
+    {
+
+        private static int[] elements;
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            elements = new int[num];
+            NestedLoops(0);
+        }
+
+        private static void NestedLoops(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", elements));
+                return;
+            }
+
+            for (int i = 1; i <= elements.Length; i++)
+            {
+                elements[index] = i;
+                NestedLoops(index + 1);
+            }
+        }
+    }
+}
